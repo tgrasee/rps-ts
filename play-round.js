c235966b39060b80aacdd3bc147bb3789861d678
tgrasee <tgrasee@gmail.com>
2022-09-09 16:27:46 -0400

    Add else if statement.

--- play-round.js
+++ play-round.js
@@ -2,10 +2,10 @@ function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
 
   let selector = Math.floor(Math.random() * 3);
-  console.log(selector);
+//   console.log(selector);
 
   let choice = choices[selector];
-  console.log(choice);
+//   console.log(choice);
 
   return choice;
 }
@@ -13,18 +13,25 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   let playerSelectionLowercase = playerSelection.toLowerCase();
   let computerSelectionLowercase = computerSelection.toLowerCase();
-
+  console.log("Player lowercase:" + playerSelectionLowercase);
+  console.log("Computer lowercase:" + computerSelectionLowercase);
   if (playerSelectionLowercase == computerSelectionLowercase) {
-    return "It's a tie!"
-  } else if 
-    (playerSelectionLowercase == "rock" && computerSelectionLowercase == "paper");
-    (playerSelectionLowercase == "paper" && computerSelectionLowercase == "scissors");
-    (playerSelectionLowercase == "scissors" && computerSelectionLowercase == "rock");
-
+    return "It's a tie!";
+  } else if (
+      (playerSelectionLowercase == "rock" && computerSelectionLowercase == "paper") ||
+      (playerSelectionLowercase == "paper" && computerSelectionLowercase == "scissors") ||
+      (playerSelectionLowercase == "scissors" && computerSelectionLowercase == "rock")
+  ) {
+      return "You lost!";
+  } else {
+      return "You won!";
+  }
+    
 }
 
-console.log(getComputerChoice());
 
-const playerSelection = "rock";
+const playerSelection = "paper";
+console.log("Player:" + playerSelection);
 const computerSelection = getComputerChoice();
+console.log("Computer:" + computerSelection);
 console.log(playRound(playerSelection, computerSelection));
