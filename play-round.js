6582240036841edc6e0d4cd6ab8a103a4a077a6e
tgrasee <tgrasee@gmail.com>
2022-09-09 13:41:31 -0400

    Add playerSelectionLowercase and computerSelectionLowercase variables. Add if statement.

--- play-round.js
+++ play-round.js
@@ -1,16 +1,22 @@
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
+  const choices = ["Rock", "Paper", "Scissors"];
 
-    let selector = Math.floor(Math.random() * 3);
-    console.log(selector);
+  let selector = Math.floor(Math.random() * 3);
+  console.log(selector);
 
-    let choice = choices[selector];
-    console.log(choice);
+  let choice = choices[selector];
+  console.log(choice);
 
-    return choice;
+  return choice;
 }
 
 function playRound(playerSelection, computerSelection) {
+  let playerSelectionLowercase = playerSelection.toLowerCase();
+  let computerSelectionLowercase = computerSelection.toLowerCase();
+
+  if (playerSelectionLowercase === "rock" && computerSelectionLowercase === "scissors") {
+    return ""
+  }
 
 }
 
